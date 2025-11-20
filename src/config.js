1d26e09bc3269b709365d9f96b2b2248a4f8c268
Jose Reyes Palacio <jrpalacio.dev@gmail.com>
2025-11-20 08:56:59 -0600

    feat(whatsapp): Actualizar la configuración de WhatsApp para usar el nuevo número de teléfono y mensaje

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
 export const shoppingCartUrl = "https://s.happylive.mx:1035/h/iCommerce/templates/Happylive/CarritoCompras/index.jsp?ID=2428";
-export const whatsappNumber = "7221083703";
+
+export const whatsapp = {
+    phone: "7223031842",
+    message: "¡Hola Lucy! Me gustaría probar los productos de Happy Live. ¿Podrías brindarme más información?"
+}
